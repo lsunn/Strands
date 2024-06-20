6d006de5302ee3aef5405c5254f98b62b9f1b173
lsunn <laurenhsun@gmail.com>
2024-06-20 02:30:13 -0700

    Finding words ticket

--- src/Board.js
+++ src/Board.js
@@ -1,15 +1,23 @@
 import React, { useState } from 'react';
 import './App.css';
+import board from './Data.js';
+// import en from 'dictionary-en';
+// import nspell from 'nspell';
 
 const cell = {
     //letter to be determined
     letter: 'A',
     isSelected: false,
     isFound: false,
+    isThemeWord: false,
+    isSpangram: false
 };
 
 const numRows = 8;
 const numCols = 6;
+// const spell = nspell(en)
+// console.log(spell.correct('color'))
+// console.log(spell.correct('colour'))
 
 const createInitialGrid = () => {
     // Initialize an empty array for the grid
@@ -18,7 +26,7 @@ const createInitialGrid = () => {
         // Initialize an empty array for the current row
         const currentRow = [];
         for (let col = 0; col < numCols; col++) {
-            currentRow.push({ ...cell });
+            currentRow.push({ ...cell, letter: board.letters[row][col] });
         }
         //Add the current row to the grid
         grid.push(currentRow);
@@ -26,32 +34,63 @@ const createInitialGrid = () => {
     return grid;
 };
 
-const Cell = ({ cell }) => {
+const Cell = ({ cell, onClick }) => {
+    let backgroundColor = 'white';
+
+    if (cell.isSelected) {
+        backgroundColor = "#D8D8C5";
+    }
+
+    if (cell.isFound) {
+        backgroundColor = cell.isThemeWord ? '#AAE5F2' : cell.isSpangram ? '#EFC929' : null;
+    }
+
     return (
-        <div className="cell">
+        <div className="cell" onClick={onClick}>
             {cell.letter}
         </div>
     );
 };
 
-function chooseLetter() {
-    
-};
-
 function Board() {
     const [grid, setGrid] = useState(createInitialGrid());
+    const [chosenLetters, setChosenLetters] = useState('');
+    
+    const handleCellClick = (rowIndex, colIndex) => {
+        const updatedGrid = [...grid];
+        const clickedCell = updatedGrid[rowIndex][colIndex];
+
+        if (!clickedCell.isSelected) {
+            setChosenLetters(prevLetters => prevLetters + cell.letter);
+        } else {
+            setChosenLetters('');
+            // if (!spell.correct(chosenLetters)) {
+            //     setChosenLetters('Not in word list');
+            // }
+            if (board.words.includes(chosenLetters.toLowerCase))
+                {
+        
+                }
+            
+        }
+
+        cell.isSelected = !cell.isSelected;
+    };
 
     return (
         <div className="grid">
+            <h1>{chosenLetters}</h1>
             {grid.map((row, rowIndex) => (
                 <div key={rowIndex} className="row">
                     {row.map((cell, colIndex) => (
-                        <Cell key={colIndex} cell={cell} onclick="chooseLetter()"/>
+                        <Cell key={colIndex} cell={cell} onClick={() => handleCellClick(rowIndex, colIndex)}/>
                     ))}
                 </div>
             ))}
         </div>
     );
 };
+
+console.log("test");
 
 export default Board;
--- src/Home.js
+++ src/Home.js
@@ -1,27 +1,25 @@
 import React from 'react';
 import strandsLogo from './images/strandsLogo.png'
 
-function Home() {
-    const handlePlayClick = () => {
-    };
-
+function Home(props) {
     return (
-    <div className="App">
-        <header className="StartScene">
-        <img src={strandsLogo} alt="Strands logo" />
-        <h1 className="Title">Strands</h1>
-        <h2>BETA</h2>
-        <p>
-            Find hidden words and uncover the day's theme
-        </p>
+        <div className="App">
+            <header className="StartScene">
+            <img className="App-logo" src={strandsLogo} alt="Strands logo" />
+            <h1 className="Title">Strands</h1>
+            <h2>BETA</h2>
+            <p>
+                Find hidden words and uncover the day's theme
+            </p>
 
-        <button onClick={handlePlayClick}>Play</button>
+            <button onClick={() => {props.setScene("board")}}>Play
+            </button>
 
-        <p>Date</p>
-        <p>Puzzle by </p>
-        </header>
-        
-    </div>
+            <p>Date</p>
+            <p>Puzzle by </p>
+            </header> 
+            
+        </div>
     );
 }
 
